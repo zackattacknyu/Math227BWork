f1faf8c7d3a7e8b1aba4489ee252f4e2a6742e64
zackattacknyu <zackattacknyu@gmail.com>
2017-01-26 23:56:01 -0800

    df

--- hw2/doNewtonMethod.m
+++ hw2/doNewtonMethod.m
@@ -1,7 +1,42 @@
-function [ output_args ] = doNewtonMethod( input_args )
+function [ curXvec2,solFound ] = doNewtonMethod( f1,f2,Df1x1,Df1x2,Df2x1,Df2x2,xv0 )
 %DONEWTONMETHOD Summary of this function goes here
-%   Detailed explanation goes here
+%   
+%
+%Similar to code on this wikipedia page:
+%   https://en.wikipedia.org/wiki/Newton's_method
 
+Jacob = @(x1,x2) [Df1x1(x1,x2) Df1x2(x1,x2);...
+    Df2x1(x1,x2) Df2x2(x1,x2)];
+
+numIter = 50;
+epsilon = 1e-4;
+tolerance = 1e-8;
+%xvValues2 = zeros(2,numIter);
+
+curXvec2 = xv0;
+solFound=false;
+for i = 1:numIter
+   
+   curX1A = curXvec2(1);
+   curX2A = curXvec2(2);
+   fxv2 = [f1(curX1A,curX2A);f2(curX1A,curX2A)];
+   curJacob2 = Jacob(curX1A,curX2A);
+   
+   if(abs(det(curJacob2))<epsilon)
+       solFound=false;
+      break; 
+   end
+   
+   deltaX = linsolve(curJacob2,-fxv2);
+   nextX2 = curXvec2 + deltaX;
+   curXvec2 = nextX2;
+   
+   if(norm(deltaX)<tolerance*norm(curXvec2))
+       solFound=true;
+       break;
+   end
+   
+end
 
 end
 
--- hw2/prob1.m
+++ hw2/prob1.m
@@ -3,7 +3,7 @@
 
 %method is as follows for multidim case
 %x_1 = x_0 - inv(Df(x_0)) f(x_0)
-
+clear all;
 f1 = @(x1,x2) sin(x1) + cos(x2);
 f2 = @(x1,x2) exp(x1)+sin(x2);
 
@@ -13,37 +13,17 @@ Df1x2 = @(x1,x2) -sin(x2);
 Df2x1 = @(x1,x2) exp(x1);
 Df2x2 = @(x1,x2) cos(x2);
 
-xv0 = [0;0];
-Jacob = @(x1,x2) [Df1x1(x1,x2) Df1x2(x1,x2);...
-    Df2x1(x1,x2) Df2x2(x1,x2)];
+N = 20;
+fresArray = [];
 
+for i = 1:N
+   xv0 = rand(2,1).*10 + 6;
+   [xvN,toAdd]  = doNewtonMethod( f1,f2,Df1x1,Df1x2,Df2x1,Df2x2,xv0 );
+   if(toAdd)
+      curF = [f1(xvN(1),xvN(2));f2(xvN(1),xvN(2))];
+      fresArray = [fresArray curF];
+   end
 
-
-numIter = 20;
-%xvValues = zeros(2,numIter);
-xvValues2 = zeros(2,numIter);
-
-%curXvec = xv0;
-curXvec2 = xv0;
-for i = 1:numIter
-    %{
-   curX1 = curXvec(1);
-   curX2 = curXvec(2);
-   fxv = [f1(curX1,curX2);f2(curX1,curX2)];
-   curJacob = Jacob(curX1,curX2);
-   
-   nextX = curXvec - curJacob\fxv;
-   xvValues(:,i) = nextX;
-   curXvec = nextX;
-   %}
-   
-   curX1A = curXvec2(1);
-   curX2A = curXvec2(2);
-   fxv2 = [f1(curX1A,curX2A);f2(curX1A,curX2A)];
-   curJacob2 = Jacob(curX1A,curX2A);
-   
-   deltaX = linsolve(curJacob,-fxv);
-   nextX2 = curXvec2 + deltaX;
-   xvValues2(:,i) = nextX2;
-   curXvec2 = nextX2;
 end
+
+maxError = max(abs(fresArray(:)))
