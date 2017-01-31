e21c0a3e12861fa50f02c172f09761df7d5b8611
zackattacknyu <zackattacknyu@gmail.com>
2017-01-30 22:37:35 -0800

    df

--- hw2/calcNextW.m
+++
@@ -1,9 +0,0 @@
-function [ nextW ] = calcNextW( func,tCurrent,wCurrent,h )
-%CALCNEXTW Summary of this function goes here
-%   Detailed explanation goes here
-
-k1 = h*func(tCurrent,wCurrent);
-k2 = h*func(tCurrent + (2/3)*h,wCurrent+(2/3)*k1);
-nextW = wCurrent + (1/4)*k1 + (3/4)*k2;
-end
-
--- hw2/doProb2OrderAccTest.m
+++ hw2/doProb2OrderAccTest.m
@@ -32,3 +32,52 @@ end
 
 end
 
+function [ orderAcc ] = getRungeKuttaOrderAccuracy( ...
+    func,funcActual,startT,endT,N1 )
+%GETRUNGEKUTTAORDERACCURACY Summary of this function goes here
+%   Detailed explanation goes here
+
+h=(endT-startT)/N1;
+errorH = doRungeKuttaLocal( func,...
+    funcActual,startT,N1,h );
+
+N2=N1*2;
+h2=(endT-startT)/N2;
+errorH2 = doRungeKuttaLocal( func,...
+    funcActual,startT,N2,h2 );
+
+orderAcc = (log(errorH)-log(errorH2))/log(2);
+end
+
+function [ maxLocError ] = doRungeKuttaLocal( func,...
+    funcActual,startT,NN,h )
+%DORUNGEKUTTA Returns the global and local trunction errors for Runge Kutta
+%               Method described
+
+tCurrent = startT;
+localErrors = zeros(1,NN);
+for i = 1:NN
+    
+    %calculate local error at this w
+    yHere = funcActual(tCurrent);
+    localNextW  = calcNextW( func,tCurrent,yHere,h );
+    tCurrent = tCurrent + h;
+    localNextY = funcActual(tCurrent);
+    localErrors(i) = abs(localNextW-localNextY);
+    
+    
+end
+
+maxLocError = max(localErrors);
+
+end
+
+function [ nextW ] = calcNextW( func,tCurrent,wCurrent,h )
+%CALCNEXTW Summary of this function goes here
+%   Detailed explanation goes here
+
+k1 = h*func(tCurrent,wCurrent);
+k2 = h*func(tCurrent + (2/3)*h,wCurrent+(2/3)*k1);
+nextW = wCurrent + (1/4)*k1 + (3/4)*k2;
+end
+
